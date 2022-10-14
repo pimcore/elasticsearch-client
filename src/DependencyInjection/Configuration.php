0108a66e391efdc38a8f55ab949b85791a6e11bc
fashxp <fashxp@users.noreply.github.com>
2022-10-14 13:18:51 +0000

    Apply php-cs-fixer changes

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -15,7 +15,6 @@
 
 namespace Pimcore\Bundle\ElasticsearchClientBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
